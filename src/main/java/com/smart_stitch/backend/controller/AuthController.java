07d9c9d6be2e1b5f8b186194f0bf443d42ab5a76
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-11-20 18:46:26 +0530

    handle corss origin..

--- src/main/java/com/smart_stitch/backend/controller/AuthController.java
+++ src/main/java/com/smart_stitch/backend/controller/AuthController.java
@@ -1,21 +1,28 @@
 package com.smart_stitch.backend.controller;
 
-import com.smart_stitch.backend.dto.AuthRequest;
-import com.smart_stitch.backend.service.AuthService;
-import com.smart_stitch.backend.util.JwtHelper;
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.smart_stitch.backend.dto.AuthRequest;
+import com.smart_stitch.backend.service.AuthService;
+import com.smart_stitch.backend.util.JwtHelper;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
